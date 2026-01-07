79bc2bb86ab8b92b72f223b2662482b84188c730
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2026-01-07 19:43:04 +0100

    icons change

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -827,7 +827,7 @@ export const Dex = new class implements ModdedDex {
 		let left = (num % 12) * 40;
 		let fainted = ((pokemon as Pokemon | ServerPokemon)?.fainted ?
 			`;opacity:.3;filter:grayscale(100%) brightness(.5)` : ``);
-		return `background:transparent url(https://raw.githubusercontent.com/NeyOkaou/pokemon-sprites/a9f39cff59cc9907139e425d293cdae1a538eb50/sprites/types/pokemonicons-sheet.png) no-repeat scroll -${left}px -${top}px${fainted}`;
+		return `background:transparent url(https://raw.githubusercontent.com/NeyOkaou/pokemon-sprites/f679d70d6c36808716b275d6037aff4288aa69ca/sprites/types/pokemonicons-sheet.png) no-repeat scroll -${left}px -${top}px${fainted}`;
 	}
 
 	getTeambuilderSpriteData(pokemon: any, dex: ModdedDex = Dex): TeambuilderSpriteData {
